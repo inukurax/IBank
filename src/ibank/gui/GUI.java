c66a2a55ddf9dfe6fec1cbd70121b79b86e6725a
Hjalte J <skjoldborg94@edb.dk>
2012-12-17 12:20:32 +0100

    GUI initial

--- src/ibank/gui/GUI.java
+++ src/ibank/gui/GUI.java
@@ -3,10 +3,22 @@
  */
 package ibank.gui;
 
+import javax.swing.JFrame;
+
 /**
  * @author Hjalte S. Jorgensen
  * @version 1.00
  */
-public class GUI {
+public class GUI extends JFrame {
+    
+	private static final int WIDTH = 400;
+	private static final int HEIGHT = 300;
+
+	public GUI() {
+		setTitle("Sample Title: Area of a Rectangle");
+		setSize(WIDTH, HEIGHT);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
 
 }
